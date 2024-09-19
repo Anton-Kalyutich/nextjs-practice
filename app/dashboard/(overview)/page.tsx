37e24498a023cc8b20a4534a42d6950a4356cac1
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-09-19 11:40:03 -0400

    LatestInvoices and Revenue Now Fetch Own Data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 // Fonts
 import { lusitana } from '@/app/ui/fonts';
 
@@ -6,12 +8,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
+// Skeletons
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+
 // Data fetching functions
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { fetchCardData } from '../../lib/data';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -35,8 +39,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart  />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
