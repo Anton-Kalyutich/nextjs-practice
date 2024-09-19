06781da6a9b7ceeb698faedeac9c48f2d3d0542f
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-09-19 13:19:41 -0400

    Implemented Debouncing on Search Input Field

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
@@ -9,7 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  const handleSearch = function (term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+   
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -17,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,6 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
